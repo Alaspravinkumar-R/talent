4530d3bb2594948b60608735691869405932b897
Alaspravinkumar-R <alaspravinkumar.rajamani@crud-operations.com>
2023-09-27 18:32:46 +0530

    netlify updates

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,22 @@
-import ServerlessHttp from "serverless-http"
-import app from "../src/app"
+const express = require("express");
+const serverless = require("serverless-http");
 
-export default ServerlessHttp(app);
+// Create an instance of the Express app
+const app = express();
+
+// Create a router to handle routes
+const router = express.Router();
+
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/api`, router);
+
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
